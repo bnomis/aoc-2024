a8c3d8ddf92957830d7175e5964bcee65ebe7dfa
Simon Blanchard <bnomis@gmail.com>
2024-12-04 23:12:26 +0100

    dynamic mod imports

--- src/aoc/run.py
+++ src/aoc/run.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
-import aoc.days.day01.run
-import aoc.days.day02.run
-import aoc.days.day03.run
+from __future__ import annotations
+
+import datetime
+import importlib
+
+
+def run_day(day: int) -> None:
+    module_name = 'aoc.days.day%02d.run' % day
+    mod = importlib.import_module(module_name)
+    mod.main()
 
 
 def main() -> None:
     print('Advent of code 2024')
-    aoc.days.day01.run.main()
-    aoc.days.day02.run.main()
-    aoc.days.day03.run.main()
+    today = datetime.date.today()
+    xmas = datetime.date(2024, 12, 25)
+    end = 25
+    if today < xmas:
+        end = today.day
+
+    for day in range(1, end + 1):
+        run_day(day)
 
 
 if __name__ == '__main__':
