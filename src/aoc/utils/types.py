89f30e47689f5fb1431c95f78a5631bd2fe84669
Simon Blanchard <bnomis@gmail.com>
2024-12-17 14:29:55 +0100

    Position type

--- src/aoc/utils/types.py
+++ src/aoc/utils/types.py
@@ -1,3 +1,33 @@
 import collections
+from typing import Self
 
 Point = collections.namedtuple('Point', ['x', 'y'])
+
+
+class Position:
+    def __init__(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        return f'{self.x},{self.y}'
+
+    def __eq__(self, value: Self) -> bool:
+        return self.x == value.x and self.y == value.y
+
+    def __neq__(self, value: Self) -> bool:
+        return self.x != value.x or self.y != value.y
+
+    def __add__(self, value: Self) -> Self:
+        return Position(self.x + value.x, self.y + value.y)
+
+    def __mul__(self, value: int) -> Self:
+        return Position(self.x * value, self.y * value)
+
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+
+    def update(self, value: Self) -> Self:
+        self.x += value.x
+        self.y += value.y
+        return self
